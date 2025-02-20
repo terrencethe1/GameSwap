c4a516877f30779708b7b945c56d4c8ac3c5c2ce
Christopher Makousky <christopher.makousky@gmail.com>
2025-02-20 11:47:57 -0600

    Added 'SwapCatalogue.ts' to 'server/src/models'.

---
+++ server/src/models/SwapCatalogue.ts
@@ -0,0 +1,29 @@
+import { Schema, model, type Document } from 'mongoose';
+
+// import schema from Game.js
+import gameSchema from './Game.js';
+import type { GameDocument } from './Game.js';
+
+export interface SwapCatalogueDocument extends Document {
+    allGames: GameDocument[];
+    availableGames: GameDocument[];
+    swappedGames: GameDocument[];
+}
+
+const swapCatalogueSchema = new Schema<SwapCatalogueDocument> (
+    {
+        allGames: [gameSchema],
+        availableGames: [gameSchema],
+        swappedGames: [gameSchema]
+    },
+    // set this to use virtual below
+    {
+        toJSON: {
+        virtuals: true,
+        },
+    }
+);
+
+const SwapCatalogue = model<SwapCatalogueDocument>('SwapCatalogue', swapCatalogueSchema)
+
+export default SwapCatalogue
