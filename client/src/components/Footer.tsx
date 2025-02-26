9bc13e37faa18ad5a4f8b20ba14ec6cb9d04f60f
Thida612 <angelic722@gmail.com>
2025-02-25 20:05:51 -0600

    add footer to the compontents file

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <p>&copy; 2025 GameSwap. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
