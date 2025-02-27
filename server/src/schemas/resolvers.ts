dc039caa2c58413000c309e4cce4b32ff39f9820
Christopher Makousky <christopher.makousky@gmail.com>
2025-02-27 15:45:52 -0600

    Formatted the indentation in 'server/src/schemas/resolvers.ts' to be consistent throughout the entire file.

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -5,14 +5,14 @@ import dayjs from 'dayjs';
 
 // Argument Types
 interface LoginUserArgs {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 interface NewUserArgs {
-    username: string;
-    email: string;
-    password: string;
+  username: string;
+  email: string;
+  password: string;
 }
 
 interface MyGamesArgs {
@@ -26,86 +26,86 @@ interface SearchBarArgs {
 
 const resolvers = {
   Query: {
-      // get a single user by either his id or his username
-      me: async (_parent: any, _args: MyGamesArgs, context: any): Promise<UserDocument | null> => {
-        if (context.user) {
-            const params = { id: context.user.id, username: context.user.username };
-            return User.findOne({
-              $or: [{ _id: params.id }, { username: params.username }],
-          }).populate({ path: 'savedGames', populate: { path: '_id' } });
-        };
-        throw new AuthenticationError('Could not authenticate user.');
-      },
-      // Retrieve an array of all the games in the GameSwap Library
-      gameSwapLibrary: async (_parent: any, _args: any): Promise<GameDocument[]> => {
-        return LibraryGame.find();
-      },
-      // A query to search for a game in the Library based on title
-      searchBar: async (_parent: any, searchArgs: SearchBarArgs): Promise<GameDocument[] | null> => {
-        return LibraryGame.find(
-          { title: { $regex: `${searchArgs.title}`, $options: 'i' } }
-        );
-      }
+    // get a single user by either his id or his username
+    me: async (_parent: any, _args: MyGamesArgs, context: any): Promise<UserDocument | null> => {
+      if (context.user) {
+          const params = { id: context.user.id, username: context.user.username };
+          return User.findOne({
+            $or: [{ _id: params.id }, { username: params.username }],
+        }).populate({ path: 'savedGames', populate: { path: '_id' } });
+      };
+      throw new AuthenticationError('Could not authenticate user.');
+    },
+    // Retrieve an array of all the games in the GameSwap Library
+    gameSwapLibrary: async (_parent: any, _args: any): Promise<GameDocument[]> => {
+      return LibraryGame.find();
+    },
+    // A query to search for a game in the Library based on title
+    searchBar: async (_parent: any, searchArgs: SearchBarArgs): Promise<GameDocument[] | null> => {
+      return LibraryGame.find(
+        { title: { $regex: `${searchArgs.title}`, $options: 'i' } }
+      );
+    }
   },
   Mutation: {
     // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
     loginUser: async (_parent: any, { email, password }: LoginUserArgs) => {
-        // Find a user with the provided email
-        const user = await User.findOne({ email });
-      
-        // If no user is found, throw an AuthenticationError
-        if (!user) {
-          throw new AuthenticationError('Could not authenticate user.');
-        }
-      
-        // Check if the provided password is correct
-        const correctPw = await user.isCorrectPassword(password);
-      
-        // If the password is incorrect, throw an AuthenticationError
-        if (!correctPw) {
-          throw new AuthenticationError('Could not authenticate user.');
-        }
-      
-        // Sign a token with the user's information
-        const token = signToken(user.username, user.email, user._id);
-      
-        // Return the token and the user
-        return { token, user };
+      // Find a user with the provided email
+      const user = await User.findOne({ email });
+    
+      // If no user is found, throw an AuthenticationError
+      if (!user) {
+        throw new AuthenticationError('Could not authenticate user.');
+      }
+    
+      // Check if the provided password is correct
+      const correctPw = await user.isCorrectPassword(password);
+    
+      // If the password is incorrect, throw an AuthenticationError
+      if (!correctPw) {
+        throw new AuthenticationError('Could not authenticate user.');
+      }
+    
+      // Sign a token with the user's information
+      const token = signToken(user.username, user.email, user._id);
+    
+      // Return the token and the user
+      return { token, user };
     },
     // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
     addUser: async (_parent: any, { username, email, password }: NewUserArgs) => {
-        const user = await User.create({ username, email, password });
-      
-        // Sign a token with the user's information
-        const token = signToken(user.username, user.email, user._id);
+      const user = await User.create({ username, email, password });
+    
+      // Sign a token with the user's information
+      const token = signToken(user.username, user.email, user._id);
 
-        return { token, user };
+      return { token, user };
     },
     // save a game to a user's `savedGames` field by adding it to the set (to prevent duplicates)
     saveGame: async (_parent: any, saveGameArgs: ObjectId, context: any) => {
-        // Generate the returnDate 14 days into the future
-        // const generateReturnDate = new Date(new Date().setDate(new Date().getDate() + 14));
+      // Generate the returnDate 14 days into the future
+      // const generateReturnDate = new Date(new Date().setDate(new Date().getDate() + 14));
 
-        // Use dayjs to generate returnDate 14 days into the future
-        const generateReturnDate = dayjs().add(14,'day').format(`MMM-DD-YYYY`);
-      
-        const rentalData = {
-          _id: saveGameArgs,
-          returnDate: generateReturnDate
-        }
+      // Use dayjs to generate returnDate 14 days into the future
+      const generateReturnDate = dayjs().add(14,'day').format(`MMM-DD-YYYY`);
+    
+      const rentalData = {
+        _id: saveGameArgs,
+        returnDate: generateReturnDate
+      }
 
-        if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $addToSet: { savedGames: rentalData } },
-            { new: true, runValidators: true }
-          ).populate({ path: 'savedGames', populate: { path: '_id' } })
-          if (!updatedUser) {
-            throw new AuthenticationError(`Cannot add ${saveGameArgs}.`);
-          };
-          return updatedUser.savedGames;
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedGames: rentalData } },
+          { new: true, runValidators: true }
+        ).populate({ path: 'savedGames', populate: { path: '_id' } })
+        if (!updatedUser) {
+          throw new AuthenticationError(`Cannot add ${saveGameArgs}.`);
         };
-        throw new AuthenticationError('Cannot find context.');
+        return updatedUser.savedGames;
+      };
+      throw new AuthenticationError('Cannot find context.');
     },
     // remove a game from `savedGames`
     removeGame: async (_parent: any, removeGameArgs: ObjectId, context: any) => {
